993fc097aa2e83ed721c482deef1e332bd7e7a42
Lilkoro <87282972+Lilkoro@users.noreply.github.com>
2021-07-12 10:10:43 +0200

    Update messageReactionRemove.js

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js')
 
-module.exports = async(client, reaction, user) => {
+module.exports = async(member, reaction, user) => {
 
-    const channel = reaction.mesage.guild.channels.cache.find(c => c.id === '834720471793795133');
+    const channel = reaction.message.guild.channels.cache.find(c => c.id === '834720471793795133');
     const Peuple = reaction.message.guild.roles.cache.get("832335337518792755");
     const yesEmoji = reaction.message.guild.emojis.cache.get('✅');
 
     if (member.user.bot) return;
 
-    if (reaction.emoji.name === ':white_check_mark:' && reaction.message.channel.id === channel.id){ 
+    if (reaction.emoji.name === '✅' && reaction.message.channel.id === channel.id){ 
         reaction.member.roles.remove(Peuple)
         reaction.message.channel.send(`Le rôle ${Peuple.name} a été supprimé avec succès !`)
     }
